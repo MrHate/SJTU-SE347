bd600c18a90df9c96542774a24f6488e1c486524
MrHate <dgyhateyou@sjtu.edu.cn>
2020-06-14 18:18:18 +0800

    removed using in datanode.cc

--- src/cpp/kvstore_datanode.cc
+++ src/cpp/kvstore_datanode.cc
@@ -8,21 +8,12 @@
 
 #include "kvstore.grpc.pb.h"
 
-using grpc::Server;
-using grpc::ServerBuilder;
-using grpc::ServerContext;
-using grpc::Status;
-
-using kvStore::HelloRequest;
-using kvStore::HelloReply;
-using kvStore::KvNodeService;
-
-class KvNodeServiceImpl final : public KvNodeService::Service {
-    Status SayHello(ServerContext* context, const HelloRequest* request, 
-                    HelloReply* reply) override {
+class KvNodeServiceImpl final : public kvStore::KvNodeService::Service {
+    grpc::Status SayHello(grpc::ServerContext* context, const kvStore::HelloRequest* request, 
+                    kvStore::HelloReply* reply) override {
         std::string suffix("datanode");
         reply->set_message(request->name() + suffix);
-        return Status::OK;
+        return grpc::Status::OK;
     }
 };
 
@@ -32,14 +23,14 @@ void RunServer() {
 
     grpc::EnableDefaultHealthCheckService(true);
     grpc::reflection::InitProtoReflectionServerBuilderPlugin();
-    ServerBuilder builder;
+    grpc::ServerBuilder builder;
     // Listen on the given address without any authentication mechanism.
     builder.AddListeningPort(server_address, grpc::InsecureServerCredentials());
     // Register "service" as the instance through which we'll communicate with
     // clients. In this case it corresponds to an *synchronous* service.
     builder.RegisterService(&service);
     // Finally assemble the server.
-    std::unique_ptr<Server> server(builder.BuildAndStart());
+    std::unique_ptr<grpc::Server> server(builder.BuildAndStart());
     std::cout << "Server listening on " << server_address << std::endl;
 
     // Wait for the server to shutdown. Note that some other thread must be
